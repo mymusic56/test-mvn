1d9d2044975715dc83f5cd816d2452e6c3b9c7de
zhangshengji <961272668@qq.com>
2022-03-10 23:05:07 +0800

    init

---
+++ src/main/java/com/mymusict/test/MvnApplication.java
@@ -0,0 +1,15 @@
+package com.mymusict.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhangshengji
+ * @since 2022/03/10 22:47
+ */
+@SpringBootApplication
+public class MvnApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MvnApplication.class, args);
+    }
+}
---
+++ src/main/java/com/mymusict/test/controller/IndexController.java
@@ -0,0 +1,19 @@
+package com.mymusict.test.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author zhangshengji
+ * @since 2022/03/10 22:52
+ */
+@RestController
+@RequestMapping("/index")
+public class IndexController {
+
+    @GetMapping("/hello")
+    public String index() {
+        return "index";
+    }
+}
